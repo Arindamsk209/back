286d21d66f26bb5c67adb2d5150dbc7ae00c6453
Arindam Singh <arindamsingh209@gmail.com>
2024-10-02 03:42:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require("mongoose");
+const User = require('./models/User');
+const Post = require('./models/Post');
+const bcrypt = require('bcryptjs');
+const app = express();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
+const salt = bcrypt.genSaltSync(10);
+const secret = 'asdfe45we45w345wegw345werjktjwertkj';
+const port = process.env.PORT || 4000;
+
+app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.connect('mongodb+srv://arindamsingh209:arindam@cluster1.29d0mug.mongodb.net/?retryWrites=true&w=majority');
+
+// Register Page
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
+});
+
+// Login Page
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    // logged in
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie('token', token).json({
+        id: userDoc._id,
+        username,
+      });
+    });
+  } else {
+    res.status(400).json('wrong credentials');
+  }
+});
+
+// User information header
+app.get('/profile', (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
+app.post('/logout', (req, res) => {
+  res.cookie('token', '').json('ok');
+});
+
+// Create Post Page
+app.post('/post', async (req, res) => {
+  const { title, summary, content, cover } = req.body; // Get cover image URL from request
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+
+    const postDoc = await Post.create({
+      title,
+      summary,
+      content,
+      cover, // Use the URL directly
+      author: info.id,
+    });
+    res.json(postDoc);
+  });
+});
+
+// Edit Post
+app.put('/post', async (req, res) => {
+  const { id, title, summary, content, cover } = req.body; // Get cover image URL from request
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json('you are not the author');
+    }
+    await postDoc.updateOne({
+      title,
+      summary,
+      content,
+      cover: cover ? cover : postDoc.cover, // Update with new URL if provided
+    });
+
+    res.json(postDoc);
+  });
+});
+
+// Show the post at home page
+app.get('/post', async (req, res) => {
+  res.json(
+    await Post.find()
+      .populate('author', ['username'])
+      .sort({ createdAt: -1 })
+      .limit(20)
+  );
+});
+
+// Post Page
+app.get('/post/:id', async (req, res) => {
+  const { id } = req.params;
+  const postDoc = await Post.findById(id).populate('author', ['username']);
+  res.json(postDoc);
+});
+
+app.listen(port, () => {
+  console.log('Server is running on port 4000');
+});
